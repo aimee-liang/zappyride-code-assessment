f27c84f771e9c372570ef4f67ed193f84f8989e4
aimee-liang <aimeeeliang@gmail.com>
2021-04-14 16:22:52 -0400

    Restructuring App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-// import {useState} from "react"
+import {useState} from "react"
 import Form from "./Components/Form"
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
+import LoadPage from "./Components/LoadPage"
 
 export default function App() {
-  // const [rate, setRate] = useState(null)
+/* formData is going to be set in state so we know that everything is complete */
+  const [formData, setFormData] = useState({})
+
+/* loadpage should not render until form is complete */
 
   return (
     <MuiThemeProvider>
---
+++ src/Components/LoadPage.jsx
@@ -0,0 +1,7 @@
+export default function LoadPage(props){
+    return (
+        <>
+            <h2>This is where our load page will return all data</h2>
+        </>
+    )
+};
