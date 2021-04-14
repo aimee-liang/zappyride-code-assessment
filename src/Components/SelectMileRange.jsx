b19912e6312565798cd265fee5ade8fdae61e02c
aimee-liang <aimeeeliang@gmail.com>
2021-04-14 18:26:21 -0400

    Working on using Material-UI Slider

--- src/Components/SelectMileRange.jsx
+++ src/Components/SelectMileRange.jsx
@@ -1,4 +1,6 @@
 import {useState} from 'react'
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 
 export default function SelectMileRange(props){
 
