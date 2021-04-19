e3f428b0afec341360d6908b5de93cafe7d7cf38
aimee-liang <aimeeeliang@gmail.com>
2021-04-19 00:12:30 -0400

    changeHandler() updated

--- src/Components/SelectHours.jsx
+++ src/Components/SelectHours.jsx
@@ -5,21 +5,28 @@ import styled from 'styled-components'
 const SelectHoursWrapper = styled.div`
     padding-bottom: 3%;
 `
+const valueText = (value) => {
+    return `${value}`
+}
 
 export default function SelectHours(props){
 
-    const [value, setValue] = useState([])
+    const [value, setValue] = useState([15, 25])
 
     const changeHandler = (event, newValue) => {
-        props.hourSetter(newValue)
+        setValue(newValue)
     }
+
+    // props.hourSetter(newValue)
 
     return (
         <SelectHoursWrapper>
             <Slider 
+            value={value}
+            getAriaValueText={valueText}
             valueLabelDisplay="auto"
             onChange={changeHandler}
-            marks={marks}
+            aria-labelledby="range-slider"
             />
         </SelectHoursWrapper>
     )
