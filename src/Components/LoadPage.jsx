65271675919fa463f4fedffd9e8eebb2faeff20c
aimee-liang <aimeeeliang@gmail.com>
2021-04-21 14:30:13 -0400

    LoadPageWrapper

--- src/Components/LoadPage.jsx
+++ src/Components/LoadPage.jsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
 const LoadPageWrapper = styled.div`
-    
+    padding-top: 3%;
+    margin: auto;
+    align-items: center;
+    width: 38vw;
 `
 
 export default function LoadPage(props){
